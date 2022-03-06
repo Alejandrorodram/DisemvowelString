116448d89d98c81235f09b9dab8da1a862498cb6
Alejandrorodram <ale1.zul@gmail.com>
2022-03-06 11:20:50 +0000

    first commit

---
+++ VowelCount/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace DisemvowelTrolls
+{
+    internal class Kata
+    {
+        public static string Disemvowel1(string str)
+        {
+            string[] vowels = { "a", "e", "i", "o", "u", "A", "E", "I", "O", "U" };
+            for (int i = 0; i < vowels.Length; i++)
+            {
+                str = str.Replace(vowels[i], "");
+            }
+            return str;
+        }
+
+        public static string Disemvowel2(string str)
+        {
+            return string.Concat(str.Where(ch => !"aeiouAEIOU".Contains(ch)));
+        }
+
+        public static string Disemvowel3(string str)
+        {
+            return Regex.Replace(str, "[aeiou]", "", RegexOptions.IgnoreCase);
+        }
+    }
+}
---
+++ VowelCount/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisemvowelTrolls
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Disemvowel1("Quitar las vocales de este texto"));
+            Console.WriteLine(Kata.Disemvowel2("Quitar las vocales de este texto"));
+            Console.WriteLine(Kata.Disemvowel3("Quitar las vocales de este texto"));
+
+        }
+    }
+}
